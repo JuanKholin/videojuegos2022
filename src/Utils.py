e877d0037385750e67a71731441b714339c9b0ec
735184 <48834236+735184@users.noreply.github.com>
2022-05-21 00:54:36 +0200

    Op IA again

--- src/Utils.py
+++ src/Utils.py
@@ -101,7 +101,7 @@ EASY = 0
 MEDIUM = 1
 HARD = 2
 NULL = 3
-DECISSION_RATE = [450, 300, 150, 10000]
+DECISSION_RATE = [400, 250, 150, 10000]
 
 # espera de la IA para tomar decisiones ligeras:
 AI_LAPSE = [25, 20, 15, 10000]
